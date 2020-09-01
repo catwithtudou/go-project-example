c938254c464dc9170ca97b1e2f9fd908f311295c
zhengyu <949812478@qq.com>
2020-09-01 21:20:57 +0800

    test(article):add the article.http

--- blog-service/internal/routers/api/v1/article.go
+++ blog-service/internal/routers/api/v1/article.go
@@ -97,35 +97,6 @@ func (a Article) List(c *gin.Context) {
 // @Failure 500 {object} errcode.Error "内部错误"
 // @Router /api/v1/articles/{id} [put]
 func (a Article) Update(c *gin.Context) {
-	param := service.CreateArticleRequest{}
-	response := app.NewResponse(c)
-	valid, errs := app.BindAndValid(c, &param)
-	if !valid {
-		global.Logger.Errorf(c, "app.BindAndValid errs: %v", errs)
-		response.ToErrorResponse(errcode.InvalidParams.WithDetails(errs.Errors()...))
-		return
-	}
-
-	svc := service.New(c.Request.Context())
-	err := svc.CreateArticle(&param)
-	if err != nil {
-		global.Logger.Errorf(c, "svc.CreateArticle err: %v", err)
-		response.ToErrorResponse(errcode.ErrorCreateArticleFail)
-		return
-	}
-
-	response.ToSuccessResponse()
-	return
-}
-
-// @Summary 删除文章
-// @Produce  json
-// @Param id path int true "文章ID"
-// @Success 200 {string} string "成功"
-// @Failure 400 {object} errcode.Error "请求错误"
-// @Failure 500 {object} errcode.Error "内部错误"
-// @Router /api/v1/articles/{id} [delete]
-func (a Article) Delete(c *gin.Context) {
 	param := service.UpdateArticleRequest{ID: convert.StrTo(c.Param("id")).MustUInt32()}
 	response := app.NewResponse(c)
 	valid, errs := app.BindAndValid(c, &param)
@@ -147,20 +118,14 @@ func (a Article) Delete(c *gin.Context) {
 	return
 }
 
-// @Summary 创建文章
-// @Produce json
-// @Param tag_id body string true "标签ID"
-// @Param title body string true "文章标题"
-// @Param desc body string false "文章简述"
-// @Param cover_image_url body string true "封面图片地址"
-// @Param content body string true "文章内容"
-// @Param created_by body int true "创建者"
-// @Param state body int false "状态"
-// @Success 200 {object} model.Article "成功"
+// @Summary 删除文章
+// @Produce  json
+// @Param id path int true "文章ID"
+// @Success 200 {string} string "成功"
 // @Failure 400 {object} errcode.Error "请求错误"
 // @Failure 500 {object} errcode.Error "内部错误"
-// @Router /api/v1/articles [post]
-func (a Article) Create(c *gin.Context) {
+// @Router /api/v1/articles/{id} [delete]
+func (a Article) Delete(c *gin.Context) {
 	param := service.DeleteArticleRequest{ID: convert.StrTo(c.Param("id")).MustUInt32()}
 	response := app.NewResponse(c)
 	valid, errs := app.BindAndValid(c, &param)
@@ -175,6 +140,41 @@ func (a Article) Create(c *gin.Context) {
 	if err != nil {
 		global.Logger.Errorf(c, "svc.DeleteArticle err: %v", err)
 		response.ToErrorResponse(errcode.ErrorDeleteArticleFail)
+		return
+	}
+
+	response.ToSuccessResponse()
+	return
+}
+
+// @Summary 创建文章
+// @Produce json
+// @Param tag_id body string true "标签ID"
+// @Param title body string true "文章标题"
+// @Param desc body string false "文章简述"
+// @Param cover_image_url body string true "封面图片地址"
+// @Param content body string true "文章内容"
+// @Param created_by body int true "创建者"
+// @Param state body int false "状态"
+// @Success 200 {object} model.Article "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/articles [post]
+func (a Article) Create(c *gin.Context) {
+	param := service.CreateArticleRequest{}
+	response := app.NewResponse(c)
+	valid, errs := app.BindAndValid(c, &param)
+	if !valid {
+		global.Logger.Errorf(c, "app.BindAndValid errs: %v", errs)
+		response.ToErrorResponse(errcode.InvalidParams.WithDetails(errs.Errors()...))
+		return
+	}
+
+	svc := service.New(c.Request.Context())
+	err := svc.CreateArticle(&param)
+	if err != nil {
+		global.Logger.Errorf(c, "svc.CreateArticle err: %v", err)
+		response.ToErrorResponse(errcode.ErrorCreateArticleFail)
 		return
 	}
 
